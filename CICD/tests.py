35b8abf9c400792c37c0d901b3842ce913406f11
Gabriel Camatta Zanotelli <55805329+Zanotelli@users.noreply.github.com>
2023-05-31 17:59:34 -0300

    UPDATE

--- CICD/tests.py
+++ CICD/tests.py
@@ -1,6 +1,7 @@
 import pytest
-import * from evolutionary
+from evolutionary import *
+
 
 def test_mutate_offspring():
     x = [5, 7, 1 , 2, 0, 3, 4, 6]
-    assert fitness_nq(X) == 8
+    assert fitness_nq(x) == 8
